ed5edc6827ca21542014d9b212668593464facbc
kirankumar <kiranb.mobio@gmail.com>
2019-04-30 12:13:15 +0530

    add prompt

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,10 @@
 console.log("Hello World");
 console.log("Good Morning Ahmedabad!");
 console.log("hello there");
+let name = 'kiran';
+if(name=='kiran'){
+	console.log("this is kiran");
+}else{
+	console.log("this is not kiran");
+}
 
